26702969a90eb9850f52420886e380b30873fd04
daffanaufal <daffanaufalathallah@gmail.com>
2022-11-01 15:11:02 +0700

    sharing 2

--- routes/api.php
+++ routes/api.php
@@ -24,6 +24,26 @@ Route::resource('halo', HeloController::class);
 
 Route::resource('books', BookController::class);
 
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+//public route
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::get('/books', [BookController::class, 'index']);
+Route::get('/Books/{id}', [BookController::class, 'ishow']);
+Route::get('/Hello', [HelloController::class, 'index']);
+Route::get('/Hello/{id}', [HelloController::class, 'show']);
+
+//protected routes
+Route::middleware('auth:sanctum')->group(function () {
+    Route::resource('books', BookController::class)->except('create', 'edit', 'show', 'index');
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::resource('hello', HelloController::class)->except('create', 'edit', 'show', 'index');
+});
+
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
